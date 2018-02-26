a6fdaa479ec2717faf4ffb58431760e1112f333d
Artnod <artnod78@users.noreply.github.com>
2018-02-26 12:52:37 +0100

    Update serverconfig.sh

--- scripts/usr/local/lib/dmpmanager/serverconfig.sh
+++ scripts/usr/local/lib/dmpmanager/serverconfig.sh
@@ -409,9 +409,9 @@ isValidOptionValue() {
 	else
 		if [ "$(type -t serverconfig_$1_Range)" = "function" ]; then
 			RANGE=$(serverconfig_$1_Range)
-		fi
+		fi 
 	fi 
-	
+	 
 	case "$TYPE" in 
 		number)
 			if [ $(isANumber "$2") -eq 0 ]; then
