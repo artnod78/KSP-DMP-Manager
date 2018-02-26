7eb65eee7cfe38b4725cb37205d590a42af8c7db
Artnod <artnod78@users.noreply.github.com>
2018-02-26 13:00:22 +0100

    Update serverconfig.sh

--- scripts/usr/local/lib/dmpmanager/serverconfig.sh
+++ scripts/usr/local/lib/dmpmanager/serverconfig.sh
@@ -408,8 +408,8 @@ isValidOptionValue() {
 		RANGE=1-${#config_allowed_values[@]}
 	else
 		if [ "$(type -t serverconfig_$1_Range)" = "function" ] ; then 
-			RANGE=$(serverconfig_$1_Range) 
-		fi 
+			RANGE=$(serverconfig_$1_Range)
+		fi
 	fi 
 	case "$TYPE" in 
 		number)
