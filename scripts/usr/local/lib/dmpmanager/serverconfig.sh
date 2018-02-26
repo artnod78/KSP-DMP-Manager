ef43c266228304ef61a6cee75580281f8c43d813
Artnod <artnod78@users.noreply.github.com>
2018-02-26 14:36:18 +0100

    Update serverconfig.sh

--- scripts/usr/local/lib/dmpmanager/serverconfig.sh
+++ scripts/usr/local/lib/dmpmanager/serverconfig.sh
@@ -429,7 +429,7 @@ isValidOptionValue() {
                     		echo "0"
                     		return 
 			fi
-            	fi 
+		fi
 		;; boolean) 
             if [ $(isABool "$2") -eq 0 ] ; then 
                 echo "0" 
