001bcad495490947b9cbb32356620040661aa586
Artnod <artnod78@users.noreply.github.com>
2018-03-10 16:25:11 +0100

    Add files via upload

--- scripts/usr/local/lib/lmm/commands/status.sh
+++ scripts/usr/local/lib/lmm/commands/status.sh
@@ -28,12 +28,24 @@ lmmCommandStatus() {
 	line "Server name:" "$(getConfigValue $1 ServerName)"
 	line "Max players:" "$(getConfigValue $1 MaxPlayers)"
 	line "Game Mode:" "$(getConfigValue $1 GameMode)"
-
+	
+	local IPATH=$(getInstancePath "$1")
+	TMPPATH=`mktemp -d`
+	sed 's/<?xml version="1.0" encoding="utf-16"?>/<?xml version="1.0" encoding="utf-8"?>/' $IPATH/Config/Settings.txt > $TMPPATH/Settings.txt
+	mv $TMPPATH/Settings.txt $IPATH/Config/Settings.txt -f
+	rm -fr $TMPPATH
+	
 	echo
 	echo "Network info:"
 	line "Port:" "$(getConfigValue $1 Port)"
 	line "Public:" "$(getConfigValue $1 RegisterWithMasterServer)"
-
+	
+	local IPATH=$(getInstancePath "$1")
+	TMPPATH=`mktemp -d`
+	sed 's/<?xml version="1.0" encoding="utf-8"?>/<?xml version="1.0" encoding="utf-16"?>/' $IPATH/Config/Settings.txt > $TMPPATH/Settings.txt
+	mv $TMPPATH/Settings.txt $IPATH/Config/Settings.txt -f
+	rm -fr $TMPPATH
+	
 	echo
 }
 
