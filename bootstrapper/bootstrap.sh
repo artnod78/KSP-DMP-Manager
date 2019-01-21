9c4ca766426a6c2d4c7a5ef0e635f04e79dcf447
Artnod <artnod78@users.noreply.github.com>
2019-01-21 16:45:18 +0100

    Update bootstrap.sh

--- bootstrapper/bootstrap.sh
+++ bootstrapper/bootstrap.sh
@@ -36,7 +36,7 @@ intro() {
 	echo
 	echo "This will install a Luna Multi Player server according to the information"
 	echo "given on:"
-	echo "   https://github.com/artnod78/KSP-DMP-Manager"
+	echo "   https://github.com/artnod78/KSP-LMP-Manager"
 	echo
 	read -p "Press enter to continue"
 	echo -e "\n=============================================================\n"
@@ -49,7 +49,7 @@ nonDebianWarning() {
 		echo "will work the installed management scripts will probably"
 		echo "fail because of missing dependencies. Make sure you check"
 		echo "the website regarding the prerequisites"
-		echo "(https://github.com/artnod78/KSP-DMP-Manager)."
+		echo "(https://github.com/artnod78/KSP-LMP-Manager)."
 		echo
 		echo "Do you want to continue anyway?"
 		select yn in "Yes" "No"; do
